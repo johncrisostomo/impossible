fc2f4db4309cb1b60f073ece9be736c57696df12
John Crisostomo <jd@snapzio.com>
2016-05-13 15:34:52 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+#Practicing React + Meteor with the Mantra spec
---
+++ client/modules/core/components/MainLayout.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Layout = ({content}) => (
+  <div>
+    <header>
+      <h1>Impossible List</h1>
+    </header>
+    <div>
+      {content()}
+    </div>
+  </div>
+);
+
+export default Layout;
