d9c4bfc1126db1055457e6f1f08ffd64cf973c42
John Crisostomo <jd@snapzio.com>
2016-05-16 09:20:14 +0800

    implements add new and list categories

--- client/modules/core/routes.jsx
+++ client/modules/core/routes.jsx
@@ -5,6 +5,8 @@ import ItemList from '../items/components/ItemList.jsx';
 import EditItem from '../items/components/EditItem.jsx';
 import NewUser from '../users/containers/NewUser.js';
 import Login from '../users/containers/Login.js';
+import CategoryList from '../items/containers/CategoryList.js';
+import NewCategory from '../items/containers/NewCategory.js';
 
 export default function (injectDeps, {FlowRouter}) {
   const MainLayoutCtx = injectDeps(Layout);
@@ -50,6 +52,24 @@ export default function (injectDeps, {FlowRouter}) {
     action() {
       Meteor.logout();
       FlowRouter.go('/');
+    }
+  });
+
+  FlowRouter.route('/categories/', {
+    name: 'categories.list',
+    action() {
+      mount(MainLayoutCtx, {
+        content: () => (<CategoryList />)
+      });
+    }
+  });
+
+  FlowRouter.route('/categories/new/', {
+    name: 'categories.new',
+    action() {
+      mount(MainLayoutCtx, {
+        content: () => (<NewCategory />)
+      });
     }
   });
 }
--- client/modules/items/containers/CategoryList.js
+++ client/modules/items/containers/CategoryList.js
@@ -1,7 +1,7 @@
 import CategoryList from '../components/CategoryList.jsx';
-import {useDeps, composeWithTracker, composeAll} from 'matra-core';
+import {useDeps, composeWithTracker, composeAll} from 'mantra-core';
 
-export const composer = ({context}, onData => {
+export const composer = ({context}, onData) => {
   const {Meteor, Collections} = context();
   if (Meteor.subscribe('categories.list').ready()) {
     const categories = Collections.Categories.find().fetch();
